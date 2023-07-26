3a9e686fcab66667e270e77615484dd3f09481bb
wagner <wagnerweb2010@gmail.com>
2023-07-26 01:00:12 -0300

    Changed description to be optional

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Todo(models.Model):
     name = models.CharField(max_length=256)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     pub_date = models.DateTimeField("Date published", auto_now_add=True)
     to_date = models.DateTimeField("Assigned date", null=True, blank=True)
     done = models.BooleanField(default=False)
